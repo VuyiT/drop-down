0f7af6cd0f0773152cba849cbd6831a8c7a59b47
Avuyile Tiki <avuyiletiki14@gmail.com>
2025-08-04 16:22:08 +0200

    Create module to toggle the menu

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import "./style.css";
+
+(function() {
+    const menuButton = document.querySelector(".menu-button");
+    const menuContentContainer = document.querySelector(".menu-content-container");
+
+    function toggleTheMenu() {
+        if(menuContentContainer.style.visibility === "visible") {
+            console.log(`The menu is now invisible`)
+            menuContentContainer.style.visibility = "hidden";
+        } else {
+            console.log(`The menu is now visible`)
+            menuContentContainer.style.visibility = "visible";
+        }
+    }
+    menuButton.addEventListener("click", toggleTheMenu);
+})();
